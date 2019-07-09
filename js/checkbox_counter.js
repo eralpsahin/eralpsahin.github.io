0d79a4052716bb600963719bf9183b29b0b48ab2
Eralp Sahin <eralpsahin@me.com>
2019-07-08 20:31:31 -0400

    rebuilding site Mon Jul  8 20:31:31 EDT 2019

---
+++ js/checkbox_counter.js
@@ -0,0 +1,8 @@
+"use strict";
+
+
+(function() {
+    let checkboxes = document.querySelectorAll("input[type='checkbox']");
+    let checked = document.querySelectorAll("input[type='checkbox']:checked");
+    console.log(`Total: ${checkboxes.length} Checked: ${checked.length}.`);
+})();
